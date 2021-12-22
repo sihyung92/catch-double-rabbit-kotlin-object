fe70e7acac51cab3e2f255cc8d9627f02459ee5a
sihyung92 <fjzjqhdl@gmail.com>
2021-12-22 16:50:13 +0900

    feat: day1. version up & use readln() method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val name = readln()
+    println("Hello, $name!")
 }
